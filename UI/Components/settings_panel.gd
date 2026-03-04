cac6b874b229aeb55e2f37165c01759b00a6857f
Patobeurre <brice@zaclys.net>
2026-03-04 02:21:53 +0100

    fix save volume preferences

--- UI/Components/settings_panel.gd
+++ UI/Components/settings_panel.gd
@@ -27,12 +27,6 @@ var custom_properties :Array[Dictionary] = [
 	},
 ]
 
-@onready var language_options: OptionButton = $MarginContainer/VBoxContainer/ScrollContainer/VBoxContainer/MarginContainer3/VBoxContainer/ResolutionContainer/Dropdown_Languages
-@onready var msaa_options = $MarginContainer/VBoxContainer/ScrollContainer/VBoxContainer/MarginContainer/VBoxContainer/AntialiasingContainer/Dropdown_AntiAliasing
-@onready var resolution_options: OptionButton = $MarginContainer/VBoxContainer/ScrollContainer/VBoxContainer/MarginContainer/VBoxContainer/ResolutionContainer/Dropdown_Resolution
-@onready var cb_fullscreen: CheckBox = $MarginContainer/VBoxContainer/ScrollContainer/VBoxContainer/MarginContainer/VBoxContainer/FullscreenContainer/CB_Fullscreen
-@onready var cb_speedrunmode :CheckBox = $MarginContainer/VBoxContainer/ScrollContainer/VBoxContainer/MarginContainer3/VBoxContainer/SpeedrunModeContainer/CB_SpeedrunMode
-
 
 func _ready() -> void:
 	_fill_msaa_options()
@@ -44,9 +38,12 @@ func _ready() -> void:
 
 	update_fullscreen()
 	update_speedrun_mode()
+	update_volume_sliders()
 
 
 # Anti-aliasing
+
+@onready var msaa_options = $MarginContainer/VBoxContainer/ScrollContainer/VBoxContainer/MarginContainer/VBoxContainer/AntialiasingContainer/Dropdown_AntiAliasing
 
 func _fill_msaa_options() -> void:
 	var current_msaa_value = ProjectSettings.get_setting("rendering/anti_aliasing/quality/msaa_3d")
@@ -68,6 +65,8 @@ func _on_dropdown_anti_aliasing_item_selected(index: int) -> void:
 
 ## Fullscreen
 
+@onready var cb_fullscreen: CheckBox = $MarginContainer/VBoxContainer/ScrollContainer/VBoxContainer/MarginContainer/VBoxContainer/FullscreenContainer/CB_Fullscreen
+
 func update_fullscreen() -> void:
 	var window_mode = ProjectSettings.get_setting("display/window/size/mode")
 	cb_fullscreen.button_pressed = (window_mode == DisplayServer.WINDOW_MODE_FULLSCREEN)
@@ -80,6 +79,8 @@ func _on_cb_fullscreen_toggled(toggled_on: bool) -> void:
 
 
 ## Resolution options
+
+@onready var resolution_options: OptionButton = $MarginContainer/VBoxContainer/ScrollContainer/VBoxContainer/MarginContainer/VBoxContainer/ResolutionContainer/Dropdown_Resolution
 
 func _fill_resolution_options() -> void:
 	var width = ProjectSettings.get_setting("display/window/size/viewport_width")
@@ -105,6 +106,8 @@ func _on_dropdown_resolution_item_selected(index: int) -> void:
 
 ## Language options
 
+@onready var language_options: OptionButton = $MarginContainer/VBoxContainer/ScrollContainer/VBoxContainer/MarginContainer3/VBoxContainer/ResolutionContainer/Dropdown_Languages
+
 func _fill_language_options() -> void:
 	var current_language_value = TranslationServer.get_locale()
 	
@@ -120,28 +123,44 @@ func _on_dropdown_languages_item_selected(index: int) -> void:
 
 # Volume
 
+@onready var master_slider :Slider = $MarginContainer/VBoxContainer/ScrollContainer/VBoxContainer/MarginContainer2/VBoxContainer/MasterVolumeContainer/MasterHSlider
+@onready var music_slider :Slider = $MarginContainer/VBoxContainer/ScrollContainer/VBoxContainer/MarginContainer2/VBoxContainer/MusicVolumeContainer/MusicHSlider
+@onready var sfx_slider :Slider = $MarginContainer/VBoxContainer/ScrollContainer/VBoxContainer/MarginContainer2/VBoxContainer/SFXVolumeContainer/SFXHSlider
+
 var master_bus_index = AudioServer.get_bus_index("Master")
 var music_bus_index = AudioServer.get_bus_index("Music")
 var sfx_bus_index = AudioServer.get_bus_index("SFX")
 
 func _on_master_h_slider_value_changed(value :float) -> void:
 	AudioServer.set_bus_volume_db(master_bus_index, linear_to_db(value))
+	_save_bus_layout()
 
 func _on_music_h_slider_value_changed(value :float) -> void:
 	AudioServer.set_bus_volume_db(music_bus_index, linear_to_db(value))
+	_save_bus_layout()
 
 func _on_sfxh_slider_value_changed(value :float) -> void:
 	AudioServer.set_bus_volume_db(sfx_bus_index, linear_to_db(value))
+	_save_bus_layout()
 
 func _on_sfxh_slider_drag_ended(value_changed:bool) -> void:
 	AudioBus.play_sfx("BTN_OK")
+
+func update_volume_sliders() -> void:
+	master_slider.value = db_to_linear(AudioServer.get_bus_volume_db(master_bus_index))
+	music_slider.value = db_to_linear(AudioServer.get_bus_volume_db(music_bus_index))
+	sfx_slider.value = db_to_linear(AudioServer.get_bus_volume_db(sfx_bus_index))
+
+func _save_bus_layout() -> void:
+	var default_bus_layout :AudioBusLayout = load("res://default_bus_layout.tres")
+	default_bus_layout = AudioServer.generate_bus_layout()
+	ResourceSaver.save(default_bus_layout, "res://default_bus_layout.tres")
 
 
 # Mouse sensitivity
 
 @onready var sensitivity_slider :HSlider = $MarginContainer/VBoxContainer/ScrollContainer/VBoxContainer/MarginContainer3/VBoxContainer/MouseSensitivityContainer/MouseSensitivityHSlider
 @onready var sensitivity_value :RichTextLabel = $MarginContainer/VBoxContainer/ScrollContainer/VBoxContainer/MarginContainer3/VBoxContainer/MouseSensitivityContainer/MouseSensitivityValue
-
 
 func _on_mouse_sensitivity_h_slider_value_changed(value :float) -> void:
 	var sensitivity :float = clampf(value, 0.1, 2.0)
@@ -160,7 +179,6 @@ func _update_sensitivity_value():
 
 @onready var fov_slider :HSlider = $MarginContainer/VBoxContainer/ScrollContainer/VBoxContainer/MarginContainer3/VBoxContainer/CameraFOVContainer2/CameraFOVHSlider
 @onready var fov_value :RichTextLabel = $MarginContainer/VBoxContainer/ScrollContainer/VBoxContainer/MarginContainer3/VBoxContainer/CameraFOVContainer2/CameraFOVValue
-
 
 func _on_camera_fovh_slider_value_changed(value :float) -> void:
 	var fov :float = clampf(value, 50, 120)
@@ -183,6 +201,8 @@ func _init_custom_settings() -> void:
 		if !ProjectSettings.has_setting(custom_prop.name):
 			ProjectSettings.add_property_info(custom_prop)
 
+
+@onready var cb_speedrunmode :CheckBox = $MarginContainer/VBoxContainer/ScrollContainer/VBoxContainer/MarginContainer3/VBoxContainer/SpeedrunModeContainer/CB_SpeedrunMode
 
 func update_speedrun_mode() -> void:
 	var toggled :bool = ProjectSettings.get_setting("custom/speedrun_mode")
