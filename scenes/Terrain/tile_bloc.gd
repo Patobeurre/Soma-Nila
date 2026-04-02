1356a6301281a05fdac8738e0f5282322a69aac2
Patobeurre <brice@zaclys.net>
2026-04-02 10:57:27 +0200

    add tile bloc decorations

--- scenes/Terrain/tile_bloc.gd
+++ scenes/Terrain/tile_bloc.gd
@@ -2,14 +2,26 @@ extends Node3D
 class_name TileBloc
 
 
-@onready var mesh = $BaseMesh
-@onready var collision = $CollisionShape3D
+@onready var mesh = %BaseMesh
+@onready var collision = %CollisionShape3D
+@onready var decoration_node = %Decoration
 @onready var dirt_material = load("res://materials/dirt_material.tres")
 
 @onready var highlight_material :ShaderMaterial = load("res://materials/highlight_material.tres")
 @onready var highlight_red_material :ShaderMaterial = load("res://materials/highlight_red_material.tres")
 
 @export var isTop :bool = true
+
+@export_group("Decoration variables")
+@export var hasDecoration :bool = true
+@export var MAX_DECO_ELEMENTS :int = 5
+@export var border_offset :float = 0.1
+
+@onready var grass_scene :PackedScene = load("res://models/decoration/grass.tscn")
+@onready var stick_scene :PackedScene = load("res://models/decoration/stick.tscn")
+@onready var pebble_scene :PackedScene = load("res://models/decoration/pebble.tscn")
+
+
 
 var isMoving :bool = false
 var move_speed :float = 0
@@ -19,11 +31,13 @@ var to_position :Vector3 = Vector3.ZERO
 
 func _ready() -> void:
 	update_material()
+	_update_decorations()
 
 
 func set_is_top(value :bool) -> void:
 	isTop = value
 	update_material()
+	_update_decorations()
 
 
 func deactivate_collision(disabled :bool):
@@ -61,3 +75,32 @@ func highlight(toHighlight :bool, is_red :bool = false):
 		mesh.material_overlay = highlight_red_material if is_red else highlight_material
 	else:
 		mesh.material_overlay = null
+
+
+func _update_decorations() -> void:
+	if !isTop or !hasDecoration:
+		Utils.remove_children(decoration_node)
+		return
+	
+	var nb_deco_elem :int = randi_range(0, MAX_DECO_ELEMENTS)
+
+	for i in range(nb_deco_elem):
+		var obj = grass_scene.instantiate()
+		decoration_node.add_child(obj)
+		obj.position = Vector3(randf_range(border_offset, 1.0 - border_offset) - 0.5, 0.5, randf_range(border_offset, 1.0 - border_offset) - 0.5)
+		obj.rotation.y = deg_to_rad(randi_range(0, 180))
+	
+	if nb_deco_elem == MAX_DECO_ELEMENTS:
+		if randi() % 10 > 7:
+			var obj = stick_scene.instantiate()
+			decoration_node.add_child(obj)
+			obj.position = Vector3(randf_range(border_offset*2, 1.0 - border_offset*2) - 0.5, 0.5, randf_range(border_offset, 1.0 - border_offset) - 0.5)
+			obj.rotation.y = deg_to_rad(randi_range(0, 180))
+	
+	if randi() % 10 > 8:
+		for i in range(randi_range(0,3)):
+			var obj = pebble_scene.instantiate()
+			decoration_node.add_child(obj)
+			obj.position = Vector3(randf_range(border_offset*2, 1.0 - border_offset*2) - 0.5, 0.5, randf_range(border_offset, 1.0 - border_offset) - 0.5)
+			obj.rotation.y = deg_to_rad(randi_range(0, 180))
+			obj.scale = Vector3.ONE * randf()
